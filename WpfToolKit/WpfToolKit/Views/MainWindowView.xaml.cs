9b47d927c7b0963c51395ee93ca48701d1c01e81
wenbozou <1045031242@qq.com>
2023-05-27 02:43:38 +0800

    调整项目工程文件目录结构

---
+++ WpfToolKit/WpfToolKit/Views/MainWindowView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfToolKit.Views
+{
+    /// <summary>
+    /// MainWindowView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
