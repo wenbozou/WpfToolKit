db862a308ad181ea14c26bb1dd2492cb9d025a83
wenbozou <1045031242@qq.com>
2023-11-08 23:06:54 +0800

    WpfToolKit.UI 库项目创建

---
+++ WpfToolKit/WpfToolKit.Foundation/Interface/IProgress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfToolKit.Foundation.Interface
+{
+    public interface IProgress
+    {
+        void OnStart(ProgressStartMsg param);
+        void OnProgressing(ProgressMsg param);
+
+        void OnSuccess(ProgressSuccessMsg param);
+
+        void OnFailed(ProgressFailedMsg param);
+
+        void OnEnd(ProgressEndMsg param);
+    }
+
+    public class ProgressMsgBase
+    { 
+    }
+
+    public class ProgressStartMsg : ProgressMsgBase
+    { }
+
+    public class ProgressMsg : ProgressMsgBase
+    { }
+
+    public class ProgressSuccessMsg : ProgressMsgBase
+    { 
+    }
+
+    public class ProgressFailedMsg : ProgressMsgBase
+    { 
+    }
+
+    public class ProgressEndMsg : ProgressMsgBase
+    { 
+    }
+
+}
---
+++ WpfToolKit/WpfToolKit.UI/Controls/ModernWindow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfToolKit.UI.Controls
+{
+    public class ModernWindow : Window
+    {
+        #region 依赖属性
+        public static readonly DependencyProperty TitleAreaTemplateProperty = DependencyProperty.Register("TitleAreaTemplate", typeof(DataTemplate), typeof(ModernWindow), new PropertyMetadata(null));
+        public DataTemplate TitleAreaTemplate
+        {
+            get { return (DataTemplate)GetValue(TitleAreaTemplateProperty); }
+            set { SetValue(TitleAreaTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleAreaHeightProperty = DependencyProperty.Register("TitleAreaHeight", typeof(int), typeof(ModernWindow), new PropertyMetadata(0));
+        public int TitleAreaHeight
+        {
+            get { return (int)GetValue(TitleAreaHeightProperty); }
+            set { SetValue(TitleAreaHeightProperty, value); }
+        }
+        #endregion
+
+        #region 鼠标拖动窗口事件处理方法
+        protected virtual void ModernWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Source is ModernWindow && e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point pt = e.GetPosition(e.Source as ModernWindow);
+                if (pt.Y >= TitleAreaHeight)
+                {
+                    return;
+                }
+
+                this.DragMove();
+            }
+        }
+        #endregion
+    }
+}
